9492ab34ff9ad2d6d3d8a10a8948923eece0e231
Oliver <oliver.harper@mosaiquegroup.com>
2021-02-03 16:56:20 +0000

    add mobx for state management

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,17 +1,17 @@
-import React, {FormEvent, useState} from 'react';
+import React, {FormEvent, useContext, useState} from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import {v4 as uuid} from 'uuid';
+import ActivityStore from '../../../app/stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
 interface IProps {
-    setEditMode: (editMode: boolean) => void;
     activity: IActivity;
-    createActivity: (activity: IActivity) => void;
-    editActivity: (activity: IActivity) => void;
-    submitting: boolean
 }
 
-export const ActivityForm: React.FC<IProps> = ({setEditMode, activity: initialFormState, createActivity, editActivity, submitting}) => {
+export const ActivityForm: React.FC<IProps> = ({activity: initialFormState}) => {
+    const activityStore = useContext(ActivityStore);
+    const {createActivity, editActivity, submitting, cancelFormOpen} = activityStore;
 
     const initaliseForm = () => {
         if (initialFormState) {
@@ -61,7 +61,7 @@ export const ActivityForm: React.FC<IProps> = ({setEditMode, activity: initialFo
                 <Form.Input name='venue' placeholder='Venue' value={activity.venue} onChange={handleInputChange} />
                 <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
                 <Button 
-                    onClick={() => setEditMode(false)} 
+                    onClick={cancelFormOpen} 
                     floated='right'
                     type='button' 
                     content='Cancel'
@@ -71,4 +71,4 @@ export const ActivityForm: React.FC<IProps> = ({setEditMode, activity: initialFo
     )
 }
 
-export default ActivityForm;
+export default observer(ActivityForm);
